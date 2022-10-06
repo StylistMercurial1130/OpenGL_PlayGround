4f0ebc203a637a848916743fb9859f963f6ad197
V.karthik <wrk.krthik@gmail.com>
2022-10-06 21:46:33 +0530

    first commit

---
+++ build/CMakeFiles/sandbox.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "D:/Repos/OpenGL_PlayGround/main.c" "CMakeFiles/sandbox.dir/main.c.obj" "gcc" "CMakeFiles/sandbox.dir/main.c.obj.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "D:/Repos/OpenGL_PlayGround/build/Dependencies/glad/CMakeFiles/glad.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/sandbox.dir/main.c.obj.d
@@ -0,0 +1,15 @@
+CMakeFiles/sandbox.dir/main.c.obj: D:\Repos\OpenGL_PlayGround\main.c \
+ D:/Repos/OpenGL_PlayGround/Dependencies/glad/include/glad/glad.h \
+ D:/Repos/OpenGL_PlayGround/Dependencies/glad/include/KHR/khrplatform.h \
+ D:/C_C++/MinGW/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include/stdint.h \
+ D:/C_C++/MinGW/mingw64/x86_64-w64-mingw32/include/stdint.h \
+ D:/C_C++/MinGW/mingw64/x86_64-w64-mingw32/include/crtdefs.h \
+ D:/C_C++/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw.h \
+ D:/C_C++/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_mac.h \
+ D:/C_C++/MinGW/mingw64/x86_64-w64-mingw32/include/_mingw_secapi.h \
+ D:/C_C++/MinGW/mingw64/x86_64-w64-mingw32/include/vadefs.h \
+ D:/C_C++/MinGW/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_directx.h \
+ D:/C_C++/MinGW/mingw64/x86_64-w64-mingw32/include/sdks/_mingw_ddk.h \
+ D:/C_C++/MinGW/mingw64/lib/gcc/x86_64-w64-mingw32/8.1.0/include/stddef.h \
+ D:/C_C++/MinGW/mingw64/x86_64-w64-mingw32/include/stddef.h \
+ D:/Repos/OpenGL_PlayGround/Dependencies/glfw-3.3.8/install/include/GLFW/glfw3.h
